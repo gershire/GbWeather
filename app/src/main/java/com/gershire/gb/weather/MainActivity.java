04c110032a27a3a3a78af6ae9ac3fffefda5b316
Yakov Grachevsky <yakovg@reali.com>
2020-04-15 14:15:39 +0300

    Save and restore activity state.

--- app/src/main/java/com/gershire/gb/weather/MainActivity.java
+++ app/src/main/java/com/gershire/gb/weather/MainActivity.java
@@ -1,20 +1,87 @@
 package com.gershire.gb.weather;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String BUNDLE_CITY = "CITY";
+    private static final String LIFECYCLE = "LIFECYCLE";
+    private static final String INPUT = "INPUT";
 
-    private final String LIFECYCLE = "LIFECYCLE";
+    private TextView tempOutput = null;
+    private AutoCompleteTextView cityInput = null;
+    private String city = "";
+    private TextView.OnEditorActionListener cityInputListener =
+            new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    boolean handled = false;
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH && tempOutput != null) {
+                        city = v.getText().toString();
+                        Log.d(INPUT, "onEditorAction: input city is " + city);
+                        updateTemp();
+                        handled = true;
+                    }
+                    return handled;
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(LIFECYCLE, "onCreate: called");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.city_list);
+        setContentView(R.layout.activity_main);
+        initView();
         showToast("Activity created");
+    }
+
+    private void initView() {
+        tempOutput = findViewById(R.id.tempLabel);
+        cityInput = findViewById(R.id.cityInput);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                WeatherService.getCities());
+        cityInput.setAdapter(adapter);
+        cityInput.setOnEditorActionListener(cityInputListener);
+    }
+
+
+    private void populateView() {
+        if (city != null) {
+            cityInput.setText(city);
+            updateTemp();
+        }
+    }
+
+    private void updateTemp() {
+        tempOutput.setText(WeatherService.getTemp(city));
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(LIFECYCLE, "onSaveInstanceState: called");
+        outState.putString(BUNDLE_CITY, city);
+        super.onSaveInstanceState(outState);
+        showToast("Activity state saved");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(LIFECYCLE, "onRestoreInstanceState: called");
+        super.onRestoreInstanceState(savedInstanceState);
+        city = savedInstanceState.getString(BUNDLE_CITY);
+        populateView();
+        showToast("Activity state restored");
     }
 
     @Override

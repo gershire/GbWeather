3fef06494313d5cc1c962b9ab1a4c2f7acb429a9
Yakov Grachevsky <yakovg@reali.com>
2020-04-29 09:29:04 +0300

    Migrate to AndroidX

--- app/src/main/java/com/gershire/gb/weather/WeatherActivity.java
+++ app/src/main/java/com/gershire/gb/weather/WeatherActivity.java
@@ -3,7 +3,7 @@ package com.gershire.gb.weather;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.gershire.gb.weather.fragment.WeatherFragment;
 
